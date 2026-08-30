cf22aedf63ff882c5b06f483e2c67b4d8d2f755d
yacobii <mah.di@live.com>
2026-08-30 10:45:26 +0330

    updatedswagger

--- app/Providers/SwaggerUiServiceProvider.php
+++ app/Providers/SwaggerUiServiceProvider.php
@@ -10,9 +10,13 @@ class SwaggerUiServiceProvider extends ServiceProvider
     public function boot() : void
     {
         Gate::define('viewSwaggerUI', function ($user = null) {
-            return in_array(optional($user)->email, [
-                //
-            ]);
+            return true;
         });
+
+//        Gate::define('viewSwaggerUI', function ($user = null) {
+//            return in_array(optional($user)->email, [
+//                //
+//            ]);
+//        });
     }
 }
